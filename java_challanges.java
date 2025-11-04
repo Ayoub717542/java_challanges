9f322fcbd7bc3b6ebb2c7a56dbe12337b4cc0744
Ayoub Hadi <AyoubHadi856@gmail.com>
2025-11-04 18:04:14 +0100

    Update java_challanges.java

--- java_challanges.java
+++ java_challanges.java
@@ -1,13 +1,112 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
+import java.lang.Math;
+//1. Bonjour Toi
+/*public class Name{
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        System.out.println("Enter username");
+        String userName = input.nextLine();
+        System.out.println("Username is: " + userName);
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
-}
+    }
+}*/
+//2. Compte à Rebours
+
+/*public class compt_rebours {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter NUM");
+        int num = Integer.parseInt(input.nextLine());
+        for(int a = num-1 ; a > 0 ; a--){
+            System.out.println(a);
+        }
+    }
+}*/
+
+//3. Jeu du Nombre Mystère
+/*
+public class jeu_du_nomber{
+    public static void main(String[] args){
+        int number = 0;
+        double rand = (int)(Math.random()*10)+1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the guessed number (1-10):");
+            while( number != rand ){
+                number = input.nextInt();
+                input = new Scanner(System.in);
+                if(number < rand){
+                    System.out.println(number + " is less than or equal to " );
+                } else if (number > rand) {
+                    System.out.println(number + " is greater than or equal to " );
+                }else{
+                System.out.println(" great you found it !!!");
+                    input.close();
+            }
+        }
+
+    }
+    }*/
+//4. Mini Distributeur (ATM)
+public class ATM {
+
+    static float balance = 0;
+    static float deposit = 0;
+    static float withdraw = 0;
+
+    static void deposit_money() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter amount to be deposit:");
+        float depositAmount = input.nextFloat();
+        balance = deposit + depositAmount;
+        System.out.println(balance);
+        System.out.println("Deposit successful. Current balance: " + balance);
+    }
+
+    static void withdraw(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter amount to be withdrawn:");
+        float withdrawAmount = input.nextFloat();
+        if (withdraw <= balance) {
+            balance = balance - withdrawAmount;
+            System.out.println("successfully withdrawon. Current balance: " + balance);
+        } else {
+            System.out.println("no money in the account");
+        }
+    }
+
+    static void affichage() {
+        System.out.println(balance);
+    }
+
+    public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int choice = 0;
+
+    do {
+        System.out.println("\n---- ATM Menu ----");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Quit");
+        System.out.print("Enter your choice: ");
+
+        choice = input.nextInt();
+
+        switch(choice){
+            case 1:affichage();
+            break;
+            case 2:deposit_money();
+            break;
+            case 3:withdraw();
+            break;
+            case 4:System.out.println("Thank you! Goodbye.");
+            break;
+            default:System.out.println("Invalid choice.");
+
+        }}while (choice != 0);
+    }
+    }
+
+
+
+
