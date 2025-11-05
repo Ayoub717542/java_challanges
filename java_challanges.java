3d27f5577a9fa9c2b301292ae5e3581f0893b03e
Ayoub Hadi <AyoubHadi856@gmail.com>
2025-11-05 15:36:27 +0100

    Update java_challanges.java

--- java_challanges.java
+++ java_challanges.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
+import java.util.Arrays;
 //1. Bonjour Toi
 /*public class Name{
     public static void main(String[] args) {
@@ -47,7 +48,7 @@ public class jeu_du_nomber{
     }
     }*/
 //4. Mini Distributeur (ATM)
-public class ATM {
+/*public class ATM {
 
     static float balance = 0;
     static float deposit = 0;
@@ -99,14 +100,75 @@ public class ATM {
             break;
             case 3:withdraw();
             break;
-            case 4:System.out.println("Thank you! Goodbye.");
+            case 0:System.out.println("Thank you! Goodbye.");
             break;
             default:System.out.println("Invalid choice.");
 
         }}while (choice != 0);
     }
-    }
+    }*/
 
+//Trouver le deuxième plus grand élément d’un tableau.
+
+/*    public class second_grand_element {
+        public static void main(String[] args) {
+            int[] tab = new int[5];
+            int max= Integer.MIN_VALUE;
+            int second_max= 0 ;
+            Scanner input = new Scanner(System.in);
+            for(int i=0;i<tab.length;i++){
+                System.out.print("Enter numbers : ");
+                tab[i]=input.nextInt();
+            }
+            for(int i = 1 ; i < tab.length;i++) {
+                if ( tab[i] > max ) {
+                    second_max = max;
+                    max=tab[i];
+                }else if (tab[i] > second_max && tab[i]<max ) {
+                    second_max=tab[i];
+                }
+            }
+            System.out.print(second_max);
+        }}*/
+
+/*public class  Rotation_Tableau {
+    public static void main(String[] args) {
+        int [] tableau = {1,2,3,4,5};
+        int temp=tableau[tableau.length-1];
+        for(int i=tableau.length-2;i>=0;i--){
+            tableau[i+1]=tableau[i];
+        }
+        tableau[0]=temp;
+        System.out.println(Arrays.toString(tableau));
+
+    }
+}*/
+
+public class Compter_Occurrences{
+    public static void main(String[] args){
+        int []tab ={1,2,2,3,5,2,3,5,1};
+        int []unique_values =new int[tab.length];
+        int []counts =new int[tab.length];
+
+        for(int i=0;i<tab.length;i++){
+            boolean exist=false;
+            for(int j=0;j<unique_values.length;j++){
+                if(tab[i]== unique_values[j]){
+                    counts[j]++;
+                    exist=true;
+                    break;
+                }else {
+                    counts[j]=1;
+                }
+            }
+
+        }
+
+System.out.println(Arrays.toString(unique_values));
+
+
+    }
+}
 
 
 
